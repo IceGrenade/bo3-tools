5f8210986e8ddcf3112a0bbf6e0b8254c4ecba13
Ice <30480948+IceGrenade@users.noreply.github.com>
2025-04-29 15:37:25 +0100

    Update GetNewPrefabs.bat

--- GetNewPrefabs.bat
+++ GetNewPrefabs.bat
@@ -9,7 +9,7 @@ set "destFolder=%~dp0"
 if not exist "%destFolder%" mkdir "%destFolder%"
 
 :: Use forfiles to select files modified within the last day
-forfiles /P "%sourceFolder%" /S /M *.map /D 0 /C "cmd /c copy @path \"%destFolder%\\\"
+forfiles /P "%sourceFolder%" /S /M *.map /D 0 /C "cmd /c copy @path \"%destFolder%\\\""
 
 endlocal
 pause
