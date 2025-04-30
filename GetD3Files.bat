0bc01b728110ecd9ea0f8a49d6500c823e0e35d6
Ice <30480948+IceGrenade@users.noreply.github.com>
2025-04-30 12:58:33 +0100

    Update GetD3Files.bat

--- GetD3Files.bat
+++ GetD3Files.bat
@@ -9,7 +9,7 @@ set "destFolder=%~dp0"
 if not exist "%destFolder%" mkdir "%destFolder%"
 
 :: Use forfiles to select files modified within the last day
-forfiles /P "%sourceFolder%" /M *.d3dbsp /D 0 /C "cmd /c copy @path \"%destFolder%\\\""
+forfiles /P "%sourceFolder%" /M *.d3dbsp /D 0 /C "cmd /c copy @path \"%destFolder%\""
 
 endlocal
 pause
