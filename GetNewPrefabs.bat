6326dff2c0069fbc51894c79f6f8171d660a1b64
Ice <30480948+IceGrenade@users.noreply.github.com>
2025-04-30 12:57:36 +0100

    Update GetNewPrefabs.bat

--- GetNewPrefabs.bat
+++ GetNewPrefabs.bat
@@ -2,14 +2,14 @@
 setlocal EnableDelayedExpansion
 
 :: Set source and destination folders
-set "sourceFolder=%TA_TOOLS_PATH%map_source"
+set "sourceFolder=%TA_TOOLS_PATH%map_source\_prefabs"
 set "destFolder=%~dp0"
 
 :: Create destination folder if it doesn't exist
 if not exist "%destFolder%" mkdir "%destFolder%"
 
 :: Use forfiles to select files modified within the last day
-forfiles /P "%sourceFolder%" /S /M *.map /D 0 /C "cmd /c copy @path \"%destFolder%\\\""
+forfiles /P "%sourceFolder%" /S /M *.map /D 0 /C "cmd /c copy @path \"%destFolder%\""
 
 endlocal
 pause
